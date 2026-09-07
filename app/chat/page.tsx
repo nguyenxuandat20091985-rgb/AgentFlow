632fad0a00794b3e2f57e07b270c17ab142dd9a7
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-07 22:37:43 +0700

    feat: add dedicated AI CEO chat screen

---
+++ app/chat/page.tsx
@@ -0,0 +1,6 @@
+import AgentFlowShell from "../../components/agentflow/AgentFlowShell";
+import CEOChat from "../../components/agentflow/CEOChat";
+
+export default function ChatPage() {
+  return <AgentFlowShell view="chat"><CEOChat /></AgentFlowShell>;
+}
