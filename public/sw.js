25da42bdc7ffe43361b8b1076e2cad06d97abea8
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-08 20:16:57 +0700

    fix: refresh storefront service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nhabep-store-v2";
+const CACHE_NAME = "nhabep-store-v3";
 const APP_SHELL = ["/website", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
