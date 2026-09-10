ffc0a551c700cec7d8c8ffd855c82ea3eee17212
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-10 13:30:43 +0700

    feat(ai1): surface Website workflow in central Workflows

--- app/workflows/page.tsx
+++ app/workflows/page.tsx
@@ -1,11 +1,13 @@
 import AgentFlowShell from "../../components/agentflow/AgentFlowShell";
 import AIWebsiteShortcut from "../../components/agentflow/AIWebsiteShortcut";
+import AIWebsiteWorkflowPanel from "../../components/agentflow/AIWebsiteWorkflowPanel";
 
 export default function WorkflowsPage() {
   return (
     <>
       <AgentFlowShell view="workflows" />
       <AIWebsiteShortcut />
+      <AIWebsiteWorkflowPanel />
     </>
   );
 }
